1c98c0fa96fcc15e52f2ed2a1897b986df9cb84c
Sean0z <seanozinsky@gmail.com>
2025-02-04 12:36:12 -0800

    Created basic map prefab

--- Assets/Scripts/cameracontroller.cs
+++ Assets/Scripts/cameracontroller.cs
@@ -6,13 +6,12 @@ public class cameracontroller : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(playerBoat.transform.position.x, playerBoat.transform.position.y, -10f);
+        transform.position = new Vector3(playerBoat.transform.position.x + 2, playerBoat.transform.position.y + 1, -10f);
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
             transform.rotation = playerBoat.transform.rotation; //(new Vector3(0f,0f,playerBoat.transform.rotation.z));
         }
