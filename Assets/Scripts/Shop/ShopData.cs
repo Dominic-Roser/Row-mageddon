01c05b9c4eb9c2a5dd2ad035ccdc976da2f4b9ca
droser <droser@cs.washington.edu>
2025-02-16 21:00:51 -0800

    added a very basic shop without currency to buy items and bring them into the players race plan

---
+++ Assets/Scripts/Shop/ShopData.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopData : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static List<string> availableItems = new List<string>{"Beer", "Torpedo", "SpeedBoost", "WaterGun"};
+    public static List<string> allItems = new List<string>{"FishingRod", "Beer", "Torpedo", "SpeedBoost", "WaterGun"};
+}
