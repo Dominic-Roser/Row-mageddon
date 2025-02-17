e2f086f47ae42bf4ea9aa08f90f31737497a067b
droser <droser@cs.washington.edu>
2025-02-16 19:07:59 -0800

    made only the levels that are unlocked clickable

--- Assets/Scripts/Over world Map/Level Loader.cs
+++ Assets/Scripts/Over world Map/Level Loader.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    public int levelnum;
     void Start()
     {
         string levelname = this.gameObject.name;
         int spaceindex = levelname.IndexOf(" ");
-        string levelnum = (spaceindex >= 0) ? levelname.Substring(spaceindex + 1) : levelname;
-        Debug.Log(levelnum);
-        if (PlayerData.playerLevel < int.Parse(levelnum)) {
+        levelnum = (spaceindex >= 0) ? int.Parse(levelname.Substring(spaceindex + 1)) : int.Parse(levelname);
+        if (!levelUnlocked()) {
             Color tempcolor = GetComponent<Image>().color;
             tempcolor.a = 1f;
             GetComponent<Image>().color = tempcolor;
@@ -22,9 +22,14 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadScene(string levelName)
     {
-        Debug.Log("Setting levelToLoad in PlayerData: " + levelName);
-        PlayerData.levelToLoad = levelName;
-        PlayerData.previousScene = SceneManager.GetActiveScene().name;
-        UnityEngine.SceneManagement.SceneManager.LoadScene("RacePlan"); 
+        if (levelUnlocked()) {
+            Debug.Log("Setting levelToLoad in PlayerData: " + levelName);
+            PlayerData.levelToLoad = levelName;
+            PlayerData.previousScene = SceneManager.GetActiveScene().name;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("RacePlan");
+        }
+    }
+    public bool levelUnlocked() {
+        return PlayerData.playerLevel >= levelnum;
     }
 }
