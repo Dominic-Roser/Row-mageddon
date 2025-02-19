f924713f33e704da01329af97fe87b94f25e1c40
droser <droser@cs.washington.edu>
2025-02-19 15:30:25 -0800

    fixed analytic definition

--- Assets/Scripts/Events/LevelStartedEvent.cs
+++ Assets/Scripts/Events/LevelStartedEvent.cs
@@ -7,6 +7,6 @@ public class LevelStartedEvent : Event
         }
         public int playerLevel { set { SetParameter("playerLevel", value); } }
         public string chosenLevel { set { SetParameter("chosenLevel", value); } }
-        public string chosenBoat { set { SetParameter("chosenLevel", value); } }
+        public string chosenBoat { set { SetParameter("chosenBoat", value); } }
         public string[] chosenPowerups { set { SetParameter("chosenPowerups", string.Join(",", value)); } }
     }
