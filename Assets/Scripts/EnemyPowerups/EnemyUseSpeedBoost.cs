038099f9073df7f7c5610ccc8ae340b6961270a0
droser <droser@cs.washington.edu>
2025-03-02 04:27:59 -0800

    maybe fixed speedboost

--- Assets/Scripts/EnemyPowerups/EnemyUseSpeedBoost.cs
+++ Assets/Scripts/EnemyPowerups/EnemyUseSpeedBoost.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 
 public class EnemyUseSpeedBoost : MonoBehaviour {
     private float originalSpeed;
+    private bool usingSpeedBoost;
     void OnEnable()
     {
-      originalSpeed = gameObject.GetComponent<enemyPath>().CurrentSpeed;
-      //Debug.Log("boost with og speed: " + originalSpeed);
-      gameObject.GetComponent<enemyPath>().CurrentSpeed += 2f;
-      StartCoroutine(ResetSpeedAfterTime(2f));
+      if(!usingSpeedBoost) {
+        originalSpeed = gameObject.GetComponent<enemyPath>().CurrentSpeed;
+        //Debug.Log("boost with og speed: " + originalSpeed);
+        gameObject.GetComponent<enemyPath>().CurrentSpeed += 2f;
+        StartCoroutine(ResetSpeedAfterTime(2f));
+        usingSpeedBoost = true;
+      }
     }
     IEnumerator ResetSpeedAfterTime(float duration)
     {
       yield return new WaitForSeconds(duration);
       gameObject.GetComponent<enemyPath>().CurrentSpeed = originalSpeed;
+      usingSpeedBoost = false;
       //Debug.Log("done with speed boost now at speed: " + gameObject.GetComponent<enemyPath>().CurrentSpeed);
       this.enabled = false;
     }
