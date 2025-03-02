03cf4a57b3977e4f4a39a3a187b0a2d0fd859bf5
droser <droser@cs.washington.edu>
2025-03-02 03:32:00 -0800

    fixed logging params

--- Assets/Scripts/Events/PowerupUsageEvent.cs
+++ Assets/Scripts/Events/PowerupUsageEvent.cs
@@ -4,9 +4,9 @@ public class PowerupUsageEvent : Event
         public PowerupUsageEvent() : base("PowerupUsageEvent")
         {
         }
-        public float x { set { SetParameter("x", value); } }
-        public float y { set { SetParameter("y", value); } }
-        public float z { set { SetParameter("z", value); } }
+        public float x { set { SetParameter("xcoord", value); } }
+        public float y { set { SetParameter("ycoord", value); } }
+        public float z { set { SetParameter("zcoord", value); } }
         public string powerup { set { SetParameter("powerup", value); } }
         public float timeInLevel { set { SetParameter("timeInLevel", value); } }
     }
