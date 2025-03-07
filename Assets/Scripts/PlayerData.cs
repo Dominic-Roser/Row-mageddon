c35bb00e80166d0ee5c85cb470cdeb0b95b0e81c
droser <droser@cs.washington.edu>
2025-03-06 22:42:21 -0800

    reset playerdata

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -5,11 +5,11 @@ using System.Linq;
 public class PlayerData : MonoBehaviour
 {
     // IMPORTANT MAKE SURE playerLevel IS SET TO 0 whem you are done testing
-    public static int playerLevel = 4; // the player's level - represented by the most recent level they have unlocked
+    public static int playerLevel = 0; // the player's level - represented by the most recent level they have unlocked
     public static string[] SelectedPowerupNames = new string[4]; // the names of the selected powerups
     // IMPORTANT MAKE SURE POWERUPS IS SET TO "FishingRod" whem you are done testing
     //, "Torpedo", "WaterGun", "Beer", "SpeedBoost"
-    public static List<string> UnlockedPowerupNames = new List<string> {"FishingRod", "SideCannon", "Whirlpool", "Beer", "WaterGun"}; // the names of the unlocked powerups always starts out with the fishing rod 
+    public static List<string> UnlockedPowerupNames = new List<string> {"FishingRod"}; // the names of the unlocked powerups always starts out with the fishing rod 
 
     //, "PurpleBoat"
     public static List<string> UnlockedBoatNames = new List<string> {"WoodenBoat"}; // the names of the unlocked powerups always starts out with the fishing rod 
