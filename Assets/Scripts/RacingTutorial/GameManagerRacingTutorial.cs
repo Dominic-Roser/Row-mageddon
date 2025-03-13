86a8f16bd0e36df9f82f156f0306a06498f1f3f9
droser <droser@cs.washington.edu>
2025-03-12 21:49:28 -0700

    made the racing tutorial boxes easier to read

--- Assets/Scripts/RacingTutorial/GameManagerRacingTutorial.cs
+++ Assets/Scripts/RacingTutorial/GameManagerRacingTutorial.cs
@@ -86,7 +86,7 @@ public class GameManagerRacingTutorial : MonoBehaviour
         {
             PowerupDisplay.SetActive(true);
             PowerupDisplay.GetComponent<RectTransform>().anchoredPosition = new Vector3(-350f, -230f, 0f);
-            DialogueBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(-290f, -130f, 0f);
+            DialogueBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(-290f, -110f, 0f);
             dialogueIndex++;
         }
         else if (dialogueIndex == 3)
